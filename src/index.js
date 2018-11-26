0e686f40e545a3a19b4556006dbb18453dee407e
portillaj <portillaj85@yahoo.com>
2018-11-25 19:39:02 -0500

    added more queries

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import { GraphQLServer } from 'graphql-yoga';
 // Type definitions (app schema)
 const typeDefs = `
   type Query {
+    add(a: Float!, b: Float!): Float!
+    greeting(name: String, position: String): String!
     me: User!
+    post: Post!
   }
 
   type User {
@@ -15,17 +18,43 @@ const typeDefs = `
     email: String!
     age: Int
   }
+
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
+  }
 `;
 
 // Resolvers
 const resolvers = {
   Query: {
+    add(parent, args, ctx, info) {
+      return args.a + args.b
+    },
+    greeting(parent, args, ctx, info) {
+      console.log(args);
+      if (args.name) {
+        return `Hello, ${args.name}! You are my favorite ${args.position}`   
+      } else {
+        'Hello!'
+      }
+    },
     me() {
       return {
         id: '123098',
         name: 'Mike Despian',
         email: 'mike@example.com',
         age: 40
+      }
+    },
+    post() {
+      return {
+        id: '4456',
+        title: 'The Shining Review',
+        body: 'The book was great',
+        published: true
       }
     }
   }
